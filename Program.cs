01e612fea062068dcdbd6d61c8908882c3197df6
Eliezer Garcia <Eliezerp.garcia@hotmail.com>
2024-10-27 21:46:23 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_POO__Clase_Persona_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Ciudad { get; set; }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Edad: {Edad} años");
+            Console.WriteLine($"Ciudad: {Ciudad}");
+        }
+
+        public int CalcularEdadEnAnios()
+        {
+            return Edad;
+        }
+    }
+
+}
